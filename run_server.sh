26cd38b96317a90384430a5c4dce47c87c9c980b
Sahib Dhanjal <dhanjalsahib@gmail.com>
2023-02-02 18:38:37 -0800

    export env for display

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,11 @@
 # https://roboticseabass.com/2021/04/21/docker-and-ros/
 
-docker run --rm -it --net=host -v ${PWD}:/home/catkin_ws/src/ ros:latest
+docker run --rm -it \
+    --net=host \
+    -e "DISPLAY=$DISPLAY" \
+    -v "/tmp/.X11-unix/:/tmp/.X11-unix/" \
+    -v ${PWD}:/home/catkin_ws/src/ \
+    ros:latest
+
+# Go into the docker container and launch
+# cd /home/catkin_ws/ && catkin build && source devel/setup.bash && roslaunch livox_camera_calib calib.launch
