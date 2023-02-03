f5ddf8a468293a5e8f2dafe60134d82f7936dacd
sdhanjal <sdhanjal@magicleap.com>
2023-02-02 23:58:16 -0800

    update server run

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,14 @@
 # https://roboticseabass.com/2021/04/21/docker-and-ros/
+XSOCK=/tmp/.X11-unix
+xhost +"local:docker@"
 
-docker run --rm -it --net=host -v ${PWD}:/home/catkin_ws/src/ -v /Users/sdhanjal/Documents/Output:/home/data/ ros:latest
+docker run -it \
+    --gpus 'all,"capabilities=compute,utility,display"' \
+    -e DISPLAY=$DISPLAY \
+    -v $XSOCK=$XSOCK \
+    -v $HOME/.Xauthority:/root/.Xauthority \
+    -v $PWD:/home/catkin_ws/src/ \
+    -v /Users/sdhanjal/Documents/Output/:/home/data/ \
+    --privileged \
+    --net=host \
+    "$@"
